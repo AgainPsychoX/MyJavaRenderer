d3b1b3a5a439b355c6c772e0fa88cf90a0ae77db
Patryk (PsychoX) Ludwikowski <patryk.ludwikowski.7@gmail.com>
2022-04-06 18:34:05 +0200

    Add classes for 2D & 3D vectors (integer & float)
    How silly it will be to work with maths and overall stuff than requires performance in Java, where you waste memory, fragment heap and GC is annoying CPU scheluder XD. I wish we did it in C/C++ or Rust, or shaders like it should be done nowadays, but sadly many (old) people glorify still Java...

---
+++ src/main/java/MyRenderer/math/Vec2i.java
@@ -0,0 +1,11 @@
+package MyRenderer.math;
+
+public class Vec2i {
+	public int x;
+	public int y;
+
+	@Override
+	public String toString() {
+		return x + " " + y;
+	}
+}
---
+++ src/main/java/MyRenderer/math/Vec3f.java
@@ -0,0 +1,12 @@
+package MyRenderer.math;
+
+public class Vec3f {
+	public float x;
+	public float y;
+	public float z;
+
+	@Override
+	public String toString() {
+		return x + " " + y + " " + z;
+	}
+}
